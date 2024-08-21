359d8067ddea0dcc435cfbd142c58186dd562721
natidemeke3 <natidemeke3@gmail.com>
2024-08-21 11:55:21 +0300

    Finished adding the files downloading feature

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket 
 import json 
 import os
+import base64
+
 clients = [
     "192.168.8.120"
 ]
@@ -22,10 +24,7 @@ full_message = json.loads(f"{get_message_from_client()}")
 
 client_name = full_message["ClientName"]
 message_type = full_message["MessageType"]
-created_folders = 0
 def create_folder(folder_path):
-    global created_folders
-    created_folders += 1
     try:
         os.makedirs(folder_path)
     except OSError as error:
@@ -35,13 +34,17 @@ def download_sub_directory_data(main_directory,directory_data):
         files = directory_data[sub_directory_folder_name]["files"]
         folders = directory_data[sub_directory_folder_name]["folders"]
         create_folder(f"{main_directory}\\{sub_directory_folder_name}")
-        print(f"{main_directory}\\{sub_directory_folder_name}")
         index = 0
         for folder in folders: 
             download_sub_directory_data(f"{main_directory}\\{sub_directory_folder_name}", directory_data[sub_directory_folder_name]["folders"][index])
             index += 1
-            
-    
+        for file in files:
+            file_name = list(file.keys())[0]
+            file_base64 = file[file_name]
+            file_bytes = base64.urlsafe_b64decode(file_base64)
+            with open(f"{main_directory}\\{sub_directory_folder_name}\\{file_name}", "wb") as f:
+                f.write(file_bytes)
+
 
 def download_directory_data(main_directory,directory_data):
     main_directory = list(directory_data.keys())[0]
@@ -54,10 +57,13 @@ def download_directory_data(main_directory,directory_data):
             folder_name = list(folder.keys())[0]  
             download_sub_directory_data(os.path.abspath(f"./{main_directory_path}"),directory_data[main_directory]["folders"][index])
             index += 1
-            
-    print(created_folders)
-        
-        
+    for file in files:
+        file_name = list(file.keys())[0]
+        file_base64 = file[file_name]
+        file_bytes = base64.urlsafe_b64decode(file_base64)
+        with open(f"{main_directory_path}\\{file_name}", "wb") as f:
+                f.write(file_bytes)
+      
 if (message_type == "updateFoldersData"):
     directories_data = full_message["DirectoriesData"]
     for directory_data in directories_data:
